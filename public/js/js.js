16a3cd677cfdf47bf5454799d909ba5cdf5ecef1
Adonisko <kristoforlovsky@gmail.com>
2017-11-04 00:48:51 +0100

    Fuuu tak asi všetko spravené na stránke filtrovanie attrakcii

--- public/js/js.js
+++ public/js/js.js
@@ -35,3 +35,35 @@ $(".js-btn-kids-minus").click(function() {
     }
     $(".js-kids-number").val(numberOfAdults);
 });
+
+$(".js-btn-search-attractions-section").click(function() {
+    document.getElementById("attractions-search-h2").style.display = "none";   
+    var button = document.getElementById("attractions-search-btn-attractions");
+    button.style.background= "#06D8FF";
+    button.style.margin= "5px 0 0 0";
+    button.style.height = '120px';
+    button.style.borderRadius = '7px 7px 0 0';
+    document.getElementById("attractions-filtering-div").style.display = "inline";
+    document.getElementById("attractions-filtering-attractions-list").style.display = "inline";
+});
+
+$(".js-attractions-filter-button").click(function() {
+    var x, i, j, k, counter = 0;
+    var filters = [];
+    var checkboxes = document.getElementsByClassName("filtering-checkbox");
+    x = document.getElementsByClassName("filterDiv");
+    for (i = 0 ; i < checkboxes.length; i++){
+        if (checkboxes[i].checked){
+            filters.push(checkboxes[i].className.split(' ')[1]);
+            } 
+    }
+    for (i = 0; i < x.length; i++){
+            for(k = 0; k < filters.length; k++){
+                if (x[i].classList.contains(filters[k])) counter++;
+            }
+            if (counter == 0) x[i].style.display = "none";
+            else  x[i].style.display = "inline";
+            counter = 0;
+        
+    }
+});
